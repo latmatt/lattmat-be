e296e4035752daa36c3e7c4827ac1e2651fc6c27
Zayar Linn Naung <zayarlinnnaung.coder@gmail.com>
2023-11-04 10:17:35 +0630

    change redirect URI flow for OAuth

--- src/main/java/solution/com/lattmat/security/domain/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/solution/com/lattmat/security/domain/OAuth2AuthenticationSuccessHandler.java
@@ -3,6 +3,7 @@ package solution.com.lattmat.security.domain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
@@ -10,6 +11,9 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
+import solution.com.lattmat.security.entity.RefreshToken;
+import solution.com.lattmat.security.service.RefreshTokenService;
+import solution.com.lattmat.security.utils.JwtUtilities;
 
 import java.io.IOException;
 
@@ -19,6 +23,14 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     @Value("${app.oauth2.redirectUri}")
     private String redirectUri;
 
+    private final JwtUtilities jwtUtilities;
+    private final RefreshTokenService refreshTokenService;
+
+    public OAuth2AuthenticationSuccessHandler(JwtUtilities jwtUtilities, RefreshTokenService refreshTokenService){
+        this.jwtUtilities = jwtUtilities;
+        this.refreshTokenService = refreshTokenService;
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
@@ -26,8 +38,12 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
         DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
 
+        String jwtToken = jwtUtilities.generateToken(user.getName(), null);
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(user.getName());
+
         responseUri = UriComponentsBuilder.fromUriString(responseUri)
-                .queryParam("userId", user.getName())
+                .queryParam("token", jwtToken)
+                .queryParam("refreshToken", refreshToken.getToken())
                 .build().toUriString();
 
         getRedirectStrategy().sendRedirect(request, response, responseUri);
